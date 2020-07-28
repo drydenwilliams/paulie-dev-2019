21cd02717fd8a3ab158d983645ff61b92e87723d
pauliescanlon <pauliescanlon@gmail.com>
2020-07-28 10:51:19 +0100

    add gatsby-source-cloundary

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,5 +58,13 @@ module.exports = {
         trackingId: "UA-76055934-4",
       },
     },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+      },
+    },
   ],
 }
